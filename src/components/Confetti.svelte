af0cf4df7ac980c2d4565f7de4e7f246da97ff05
rjhoskins <robertjhoskins@gmail.com>
2023-01-04 21:47:31 -0800

    add confetti

---
+++ src/components/Confetti.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+  
+    let controlsLayout = [
+      'previousFrame',
+      'playpause',
+      'stop',
+      'nextFrame',
+      'progress',
+      'frame',
+      'loop',
+      'spacer',
+      'background',
+      'snapshot',
+      'zoom',
+      'info',
+    ];
+  </script>
+  
+  <div class="absolute top-0 -translate-x-1/2 left-1/2">
+    <LottiePlayer
+    src="https://assets9.lottiefiles.com/packages/lf20_rovf9gzu.json"
+      autoplay="{true}"
+      loop="{true}"
+      background="transparent"
+      height="{600}"
+      width="{600}"
+    />
+  </div>
