ba5e61eeed7381f10f74e9cbef6cb0174c2b445f
rjhoskins <robertjhoskins@gmail.com>
2023-01-09 14:29:13 -0800

    add proper meta info

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,9 @@
 
     import RoiCalculator from './components/RoiCalculator.svelte';
 	import Tailwindcss from './components/Tailwindcss.svelte';
-	export let name: string;
+
 </script>
-<style>
-	.custom-style {
-		@apply italic;
-	}
-</style>
+
 <Tailwindcss />
 <main class="flex-col items-center justify-center min-h-screen mx-auto md:flex font align-center">
 	
